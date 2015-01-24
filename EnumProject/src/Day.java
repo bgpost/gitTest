3a92d358cde30de10243d95d0faa9ddc2660e902
Bjoern Guenter <bjoern.guenter@posteo.de>
2015-01-24 13:34:55 +0100

    Added Saturday via Hotfix

--- EnumProject/src/Day.java
+++ EnumProject/src/Day.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.text.WordUtils;
 
 public enum Day {
 
-    MONDAY("Great", Locale.GERMANY), TUESDAY("Even better", Locale.CANADA), WEDNESDAY("Okay ", Locale.US), THURSDAY("like something is missing", Locale.GERMANY), FRIDAY("done", Locale.CHINESE);
+    MONDAY("Great", Locale.GERMANY), TUESDAY("Even better", Locale.CANADA), WEDNESDAY("Okay ", Locale.US), THURSDAY("like something is missing", Locale.GERMANY), FRIDAY("done", Locale.CHINESE), SATURDAY("relaxed", Locale.FRENCH);
 
     private String feeling;
     private Locale locale;
