0f905519e2baa833a2bc6a08ef3ff43e12f29d9b
Bjoern Guenter <bjoern.guenter@posteo.de>
2015-01-24 07:03:51 +0100

    Added third comment

--- EnumProject/src/TestApp.java
+++ EnumProject/src/TestApp.java
@@ -18,6 +18,7 @@ public final class TestApp {
     }
     // My first comment
     // Here comes the second comment
+    // And a third comment
 }
 
 // This is the super important comment in the TestApp
